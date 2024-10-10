defd6113d1632162d928e867fa6f21e3eb64297b
Clara260508 <148060242+Clara260508@users.noreply.github.com>
2024-10-10 07:53:09 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Filme',    
+    'Qual foi o primeiro live action da Disney?',
+    'O livro da Selva',
+)
+
+criaCartao(
+    'Disney',
+    'Quem foi a primeira princesa da Disney?',
+    'Branca de neve',
+)
+
+criaCartao(
+    'Filmes',
+    'Nos filmes Transformers, qual é o nome da  facçao do robôs vilões?',
+    'Decepticons',
+)
+
+criaCartao(
+    'Filme',
+    'Qual é o nome do personagem que odeia o Natal, interpretado por Jim Carrey?',
+    'Grinch',
+)
+
+criaCartao(
+    'Literatura',
+    'Qual livro de Machado de Assis é o mais lido?',
+    'Memórias Póstumas de Brás Cubas',
+)
+
+criaCartao(
+    'Literatura',
+    'Qual o livro mais lido de Colleen Hoover?',
+    'É assim que acaba',
+)
